4c76af20e79a84e5903d408f63afe305fa987539
Simon Siva <simonsiva13@gmail.com>
2019-03-11 15:21:42 +0300

    Update they.py

---
+++ they.py
@@ -0,0 +1,3 @@
+git merge --no-ff -m 'Merged in hotfix (pull request #2)' remotes/origin/hotfix
+git merge --no-ff -m 'Merged in hotfix (pull request #2)' remotes/origin/hotfix
+vgit merge --no-ff -m 'Merged in hotfix (pull request #2)' remotes/origin/hotfix
